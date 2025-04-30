50651bb5732ace26d181e6594088e0db01774559
Muhammad Iqbal <66395282+Iqbaladudu@users.noreply.github.com>
2025-04-30 14:17:51 +0300

    Update route.ts

--- src/app/(frontend)/api/chat/route.ts
+++ src/app/(frontend)/api/chat/route.ts
@@ -2,5 +2,5 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
  return NextResponse.json({ result: "hehe" }, { status: 200 });
-  }
+  
 }
